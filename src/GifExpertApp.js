e7307400d02450ed15921159a61146eb978039fd
EdwinTocagon <edwin.tocagon@outlook.com>
2022-01-05 19:21:58 -0500

    chnge AddCategoriy to acept category duplicate

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,29 +1,22 @@
-import React, { useState } from 'react'
-import { AddCategory } from './components/AddCategory';
-import { GifGrid } from './components/GifGrid';
+import React, { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
-export const GifExpertApp = ({ defaultCategories = [] }) => {
-    
-    // const [categories, setCategories] = useState(['One Punch']);
-    const [categories, setCategories] = useState( defaultCategories );
+export const GifExpertApp = ({ defaultCategories = ["Computador"] }) => {
+  // const [categories, setCategories] = useState(['One Punch']);
+  const [categories, setCategories] = useState(defaultCategories);
 
-    return (
-        <>
-            <h2>GifExpertApp</h2>
-            <AddCategory setCategories={ setCategories } />
-            <hr />
+  return (
+    <>
+      <h2>Buscador de Gifs</h2>
+      <AddCategory categories={categories} setCategories={setCategories} />
+      <hr />
 
-            <ol>
-                {
-                    categories.map( category  => (
-                        <GifGrid 
-                            key={ category }
-                            category={ category }
-                        />
-                    ))
-                }
-            </ol>
-
-        </>
-    )
-}
+      <ol>
+        {categories.map((category) => (
+          <GifGrid key={category} category={category} />
+        ))}
+      </ol>
+    </>
+  );
+};
